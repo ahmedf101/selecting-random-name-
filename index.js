f9fcb6a6d655a2d3f9203db38123a0f6de32ef55
ahmed farid <167867735+ahmedf101@users.noreply.github.com>
2024-05-27 12:29:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var names = ["Ahmed ","Ali","Usman","Bilal","Umer","Hassan"];
+var randomIndex = Math.floor(Math.random()*names.length);
+var randomName = names[randomIndex];
+console.log("Today "+ randomName+" is going to pay for lunch");
